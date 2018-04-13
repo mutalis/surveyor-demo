080af8771694b55bb1f9087091ff9ede6e73b964
rasg <rodolfo@mutalis.com>
2018-04-12 22:49:15 -0500

    Surveyor Demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SurveyorDemo::Application.config.secret_key_base = 'b816dc99bfe7f0017765902cbbc10aff54b6228a960b02f08f181f83455fde15631e7045baf3f5564e5df95ad4cc29f5aa404010eaab9f8970064ec74baa5926'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SurveyorDemo::Application.config.session_store :cookie_store, key: '_surveyor-demo_session'
